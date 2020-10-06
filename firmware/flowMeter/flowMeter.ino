762ca4ff00f460d2259481a3f7437ad67ac9da64
jetscholar <mrz@jetscholar.com>
2020-10-06 15:10:41 +1100

    author routes

--- firmware/flowMeter/flowMeter.ino
+++ firmware/flowMeter/flowMeter.ino
@@ -3,9 +3,11 @@
  *
  * running on: theta [192.168.1.207] - Water Pump
  * 
- * version 1.0.0
+ * version 1.0.1
  * 
- * ver 1.0.0 - 25.1.2020
+ * 1.0.1 - 5.10.2020 - stripped flow meter code, added rest function to setup
+ * 1.0.0 - 25.1.2020 - initial version
+ * 
  */
  
 // Import required libraries
@@ -17,62 +19,37 @@
 aREST rest = aREST();
 
 // WiFi parameters
-const char* ssid = "proxima";
-const char* password = "torch3helene";
+const char* ssid = "ZetaReticula";
+const char* password = "0488425378";
 
 #define LISTEN_PORT  80
-#define PULSE_PIN D2  //gpio4
 
 // Create an instance of the server
 WiFiServer server(LISTEN_PORT);
 
 // Variables to be exposed to the API
 char* location = "Water Pump";
-volatile long pulseCount=0;
-float calibrationFactor = 4.5;
-float flowRate;
-unsigned int flowMilliLitres;
-unsigned long totalMilliLitres;
-float totalLitres;
-unsigned long oldTime;
+
 
 // Declare functions to be exposed to the API
 int relayControl(String command);
 
-void ICACHE_RAM_ATTR pulseCounter()
-{
-  pulseCount++;
-}
 
 void setup() {
   // Serial Comunication init
   Serial.begin(115200);
   delay(10);
 
-  // Initialization
-  pinMode(PULSE_PIN, INPUT);
-  digitalWrite(PULSE_PIN, HIGH);
-
-  pulseCount        = 0;
-  flowRate          = 0.0;
-  flowMilliLitres   = 0;
-  totalMilliLitres  = 0;
-  oldTime           = 0; 
-
-  // The Hall-effect sensor is connected to pin 2 which uses interrupt 0.
-  // Configured to trigger on a FALLING state change (transition from HIGH
-  // state to LOW state)
-  attachInterrupt(PULSE_PIN, pulseCounter, FALLING);
-
-
     // Init variables and expose them to REST API
-    rest.variable("flowRate", &flowRate);
-    rest.variable("totalLitres", &totalLitres);
+
     rest.variable("location", &location);
 
     // Give name and ID to device
     rest.set_id("207");
     rest.set_name("theta207");
+
+    // Function to be exposed
+    rest.function("On",relayControl);
   
     // Connect to WiFi
     WiFi.begin(ssid, password);
@@ -106,42 +83,7 @@ void setup() {
 }
 
 void loop() {
-    // Read flow meter
-    //flowRate = dataRead(D2);
-  
-
-// ********
-   if((millis() - oldTime) > 1000)    // Only process counters once per second
-  {
-    detachInterrupt(PULSE_PIN);
-    flowRate = ((1000.0 / (millis() - oldTime)) * pulseCount) / calibrationFactor;
-    oldTime = millis();
-    flowMilliLitres = (flowRate / 60) * 1000;
-    totalMilliLitres += flowMilliLitres;     
-    totalLitres = totalMilliLitres * 0.001;
-    unsigned int frac;   
-    Serial.print("flowrate: ");
-    Serial.print(int(flowRate));  // Print the integer part of the variable
-    Serial.print(".");             // Print the decimal point
-    frac = (flowRate - int(flowRate)) * 10;
-    Serial.print(frac, DEC) ;      // Print the fractional part of the variable
-    Serial.print("L/min");
-    Serial.print("  Current Liquid Flowing: ");             // Output separator
-    Serial.print(flowMilliLitres);
-    Serial.print("mL/Sec");
-    Serial.print("  Output Liquid Quantity: ");             // Output separator
-    Serial.print(totalLitres);
-    Serial.println("L");
-    //Serial.print(totalMilliLitres);
-    //Serial.println("mL");
-
-    pulseCount = 0;
-
-    attachInterrupt(PULSE_PIN, pulseCounter, FALLING);
-  }
-// **********
-
-    
+ 
 
      // Handle REST calls
     WiFiClient client = server.available();
--- routes/authors.js
+++ routes/authors.js
@@ -1,11 +1,23 @@
 const express = require('express')
-const author = require('../models/author')
 const router = express.Router()
 const Author = require('../models/author')
 
-// All authors route
-router.get('/', (req, res) => {
-    res.render('authors/index')
+//All authors route
+router.get('/', async (req, res) => {
+    // set up object literal and use regex for search function
+    let searchOptions = {}
+    if (req.query.name != null && req.query.name !== '') {
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+    try { 
+        const authors = await Author.find(searchOptions)
+        res.render('authors/index', { 
+            authors: authors,
+            searchOptions: req.query
+        })
+    } catch {
+        res.redirect('/')
+    }  
 })
 
 //New author route  
